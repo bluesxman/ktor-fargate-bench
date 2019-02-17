a41cfe0c361af51447b2204767ef59a12380e155
Jon Newton <jon.newton@gmail.com>
2019-02-17 14:21:48 -0700

    working get json from s3

--- src/com/smackwerks/kfb/Main.kt
+++ src/com/smackwerks/kfb/Main.kt
@@ -6,12 +6,31 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider
+import software.amazon.awssdk.core.sync.ResponseTransformer
+import software.amazon.awssdk.regions.Region
+
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.GetObjectRequest
 
 fun main(args: Array<String>) {
     val server = embeddedServer(Netty, 8080) {
         routing {
             get("/") {
                 call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+
+            get("/s3") {
+                val creds = ProfileCredentialsProvider.builder().profileName("ktor-admin").build()
+                val s3 = S3Client.builder().region(Region.US_EAST_1).credentialsProvider(creds).build()
+                val get = GetObjectRequest.builder()
+                    .bucket("com.smackwerks-kfb")
+                    .key("hello.json")
+                    .build()
+                val json = s3
+                    .getObject(get, ResponseTransformer.toBytes())
+                    .asUtf8String()
+                call.respondText(json, ContentType.Application.Json)
             }
         }
     }
