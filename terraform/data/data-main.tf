fb4ce1d2ca40d40dd99b9adb9e6b3d6b4078c810
Jon Newton <jon.newton@gmail.com>
2019-02-17 08:27:32 -0700

    data bucket encryption

--- terraform/data/data-main.tf
+++ terraform/data/data-main.tf
@@ -18,6 +18,14 @@ resource "aws_s3_bucket" "data" {
   bucket = "${local.bucket_name}"
   acl    = "private"
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = {
       Description = "Data source for ktor-fargate-bench"
       Project = "ktor-fargate-bench"
