24d70997a72888a2d801cf17b3b716309e97df87
Jon Newton <jon.newton@gmail.com>
2019-02-24 09:48:41 -0700

    setup ECR

--- terraform/compute/compute-main.tf
+++ terraform/compute/compute-main.tf
@@ -1,10 +1,27 @@
 terraform {
   backend "s3" {
-    key    = "compute.tfstate"
+    key    = "kfb/compute.tfstate"
     region = "us-east-1"
   }
 }
 
+provider "aws" {
+  region = "${var.region}"
+}
+
+locals {
+  project = "kfb"
+}
+
 resource "aws_ecr_repository" "kfb" {
   name = "com.smackwerks.kfb.ecr"
+
+  tags {
+    Name = "${local.project}-ecr"
+    Project = "${local.project}"
+  }
+}
+
+output "ecr_url" {
+  value = "${aws_ecr_repository.kfb.repository_url}"
 }
