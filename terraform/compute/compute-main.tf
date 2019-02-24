9bfde46a27132a7121c795f6c218998b2b2d4240
Jon Newton <jon.newton@gmail.com>
2019-02-24 11:12:29 -0700

    configured load balancer

--- terraform/compute/compute-main.tf
+++ terraform/compute/compute-main.tf
@@ -12,6 +12,8 @@ provider "aws" {
 locals {
   project = "kfb"
   vpc_id = "${data.terraform_remote_state.network.vpc_id}"
+  public_subnets = "${data.terraform_remote_state.network.public_subnet_ids}"
+  private_subnets = "${data.terraform_remote_state.network.private_subnet_ids}"
 }
 
 data "terraform_remote_state" "network" {
@@ -83,6 +85,43 @@ resource "aws_security_group" "ecs_tasks" {
   tags {
     Name = "${local.project}-sg-ecs-tasks"
     Project = "${local.project}"
+  }
+}
+
+### Load Balancer
+resource "aws_alb" "main" {
+  name            = "cb-load-balancer"
+  subnets         = ["${local.public_subnets}"]  # beware of list bugs:  https://github.com/hashicorp/terraform/issues/13869
+  security_groups = ["${aws_security_group.lb.id}"]
+}
+
+resource "aws_alb_target_group" "app" {
+  name        = "cb-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = "${local.vpc_id}"
+  target_type = "ip"
+
+  health_check {
+    healthy_threshold   = "3"
+    interval            = "30"
+    protocol            = "HTTP"
+    matcher             = "200"
+    timeout             = "3"
+    path                = "/"
+    unhealthy_threshold = "2"
+  }
+}
+
+resource "aws_alb_listener" "front_end" {
+  load_balancer_arn = "${aws_alb.main.id}"
+  port              = "3000"
+  protocol          = "HTTP"
+
+  # Redirect all traffic from the ALB to the target group
+  default_action {
+    target_group_arn = "${aws_alb_target_group.app.id}"
+    type             = "forward"
   }
 }
 
