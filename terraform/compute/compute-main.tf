6e1543b8edf11fd1b4cc9abe5f7e9916876de8c1
Jon Newton <jon.newton@gmail.com>
2019-03-02 15:51:21 -0700

    added log groups; successful task start

--- terraform/compute/compute-main.tf
+++ terraform/compute/compute-main.tf
@@ -272,6 +272,23 @@ resource "aws_ecs_service" "main" {
   ]
 }
 
+# logs.tf
+
+# Set up cloudwatch group and log stream and retain logs for 30 days
+resource "aws_cloudwatch_log_group" "kfb" {
+  name              = "/ecs/${local.project}-app"
+  retention_in_days = 30
+
+  tags {
+    Name = "${local.project}-log-group"
+  }
+}
+
+resource "aws_cloudwatch_log_stream" "kfb" {
+  name           = "${local.project}-log-stream"
+  log_group_name = "${aws_cloudwatch_log_group.kfb.name}"
+}
+
 ### Outputs
 output "ecr_url" {
   value = "${aws_ecr_repository.kfb.repository_url}"
