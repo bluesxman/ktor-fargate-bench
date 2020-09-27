50104f861697512f59794ff86476ce1ddedab745
Jon Newton <jon.newton@gmail.com>
2020-09-27 11:45:44 -0600

    upgrade to tf 0.12

--- terraform/data/data-main.tf
+++ terraform/data/data-main.tf
@@ -6,17 +6,16 @@ terraform {
 }
 
 provider "aws" {
-  region = "${var.region}"
+  region = var.region
 }
 
-
 locals {
-  project = "kfb"
+  project     = "kfb"
   bucket_name = "com.smackwerks-kfb"
 }
 
 resource "aws_s3_bucket" "data" {
-  bucket = "${local.bucket_name}"
+  bucket = local.bucket_name
   acl    = "private"
 
   server_side_encryption_configuration {
@@ -29,25 +28,25 @@ resource "aws_s3_bucket" "data" {
 
   tags = {
     Description = "Data source for ktor-fargate-bench"
-    Project = "kfb"
-    Name = "KfbDataBucket"
+    Project     = "kfb"
+    Name        = "KfbDataBucket"
   }
 }
 
 resource "aws_ecr_repository" "kfb" {
   name = "com.smackwerks.${local.project}"
 
-  tags {
-    Name = "${local.project}-ecr"
-    Project = "${local.project}"
+  tags = {
+    Name    = "${local.project}-ecr"
+    Project = local.project
   }
 }
 
 resource "aws_dynamodb_table" "kfb-movies" {
-  name             = "Movies"
-  hash_key         = "year"
-  range_key        = "title"
-  billing_mode     = "PAY_PER_REQUEST"
+  name         = "Movies"
+  hash_key     = "year"
+  range_key    = "title"
+  billing_mode = "PAY_PER_REQUEST"
 
   server_side_encryption {
     enabled = false
@@ -63,16 +62,16 @@ resource "aws_dynamodb_table" "kfb-movies" {
     type = "S"
   }
 
-  tags {
-    Name = "${local.project}-dynamo-movies"
-    Project = "${local.project}"
+  tags = {
+    Name    = "${local.project}-dynamo-movies"
+    Project = local.project
   }
 }
 
 resource "aws_dynamodb_table" "kfb-movies-partition" {
-  name             = "MoviesPartitionOnly"
-  hash_key         = "title"
-  billing_mode     = "PAY_PER_REQUEST"
+  name         = "MoviesPartitionOnly"
+  hash_key     = "title"
+  billing_mode = "PAY_PER_REQUEST"
 
   server_side_encryption {
     enabled = false
@@ -83,9 +82,9 @@ resource "aws_dynamodb_table" "kfb-movies-partition" {
     type = "S"
   }
 
-  tags {
-    Name = "${local.project}-dynamo-movies-partition-only"
-    Project = "${local.project}"
+  tags = {
+    Name    = "${local.project}-dynamo-movies-partition-only"
+    Project = local.project
   }
 }
 
@@ -96,32 +95,33 @@ resource "aws_cloudwatch_log_group" "kfb" {
   name              = "/ecs/${local.project}-app"
   retention_in_days = 30
 
-  tags {
+  tags = {
     Name = "${local.project}-log-group"
   }
 }
 
 resource "aws_cloudwatch_log_stream" "kfb" {
   name           = "${local.project}-log-stream"
-  log_group_name = "${aws_cloudwatch_log_group.kfb.name}"
+  log_group_name = aws_cloudwatch_log_group.kfb.name
 }
 
 output "bucket_id" {
-  value = "${aws_s3_bucket.data.id}"
+  value = aws_s3_bucket.data.id
 }
 
 output "bucket_arn" {
-  value = "${aws_s3_bucket.data.arn}"
+  value = aws_s3_bucket.data.arn
 }
 
 output "bucket_domain_name" {
-  value = "${aws_s3_bucket.data.bucket_domain_name}"
+  value = aws_s3_bucket.data.bucket_domain_name
 }
 
 output "ecr_url" {
-  value = "${aws_ecr_repository.kfb.repository_url}"
+  value = aws_ecr_repository.kfb.repository_url
 }
 
 output "ecr_arn" {
-  value = "${aws_ecr_repository.kfb.arn}"
+  value = aws_ecr_repository.kfb.arn
 }
+
