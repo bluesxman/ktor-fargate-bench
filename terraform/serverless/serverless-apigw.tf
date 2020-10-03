1c9600c6df47fde3f472aeaf151945d051e4b121
Jon Newton <jon.newton@gmail.com>
2020-10-03 07:41:27 -0600

    define api gateway with swagger

--- terraform/serverless/serverless-apigw.tf
+++ terraform/serverless/serverless-apigw.tf
@@ -1,66 +1,26 @@
+provider "template" {
+  version = "~> 2.1"
+}
+
 resource "aws_api_gateway_rest_api" "example" {
   name        = "ServerlessExample"
   description = "Terraform Serverless Application Example"
+  body        = data.template_file.example_swagger.rendered
 }
 
-resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
-  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
-  path_part   = "{proxy+}"
-}
+data "template_file" example_swagger {
+  template = file("../../swagger.yaml")
 
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = aws_api_gateway_rest_api.example.id
-  resource_id   = aws_api_gateway_resource.proxy.id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
-  resource_id = aws_api_gateway_method.proxy.resource_id
-  http_method = aws_api_gateway_method.proxy.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.example.invoke_arn
-}
-
-resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = aws_api_gateway_rest_api.example.id
-  resource_id   = aws_api_gateway_rest_api.example.root_resource_id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
-  resource_id = aws_api_gateway_method.proxy_root.resource_id
-  http_method = aws_api_gateway_method.proxy_root.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.example.invoke_arn
+  vars = {
+    lambda_invoke_arn = aws_lambda_function.example.invoke_arn
+  }
 }
 
 resource "aws_api_gateway_deployment" "example" {
-  depends_on = [
-    aws_api_gateway_integration.lambda,
-    aws_api_gateway_integration.lambda_root,
-  ]
-
   rest_api_id = aws_api_gateway_rest_api.example.id
-  stage_name  = "test"
+  stage_name  = "default"
 }
 
-resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.example.arn
-  principal     = "apigateway.amazonaws.com"
-
-  # The /*/* portion grants access from any method on any resource
-  # within the API Gateway "REST API".
-  source_arn = "${aws_api_gateway_deployment.example.execution_arn}/*/*"
+output "invoke-url" {
+  value = aws_api_gateway_deployment.example.invoke_url
 }
-
